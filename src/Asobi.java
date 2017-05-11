901389e9a62ac9f3c09357d389055f9b8c0471d7
FUYUKI ONO <FUYUKI ONO>
2017-05-11 15:39:37 +0900

    litte regret...

--- src/Asobi.java
+++ src/Asobi.java
@@ -3,5 +3,6 @@ public class Asobi {
 	public static void main(String[] arg){
 		System.out.println("Yeah.");
 		System.out.println("Life is an easy game.");
+		System.out.println("I was fool to say life is an esay game.");
 	}
 }
